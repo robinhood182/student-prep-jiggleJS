b75f6e0ba34b202a6ed64fa743d383359b4766e9
Robyn Navarro <rnavarro182@gmail.com>
2019-01-09 14:21:41 -0800

    changed blue button to a random color instead

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-//https://stackoverflow.com/questions/3968593/how-can-i-set-multiple-css-styles-in-javascript/47136113
+//from : https://stackoverflow.com/questions/3968593/how-can-i-set-multiple-css-styles-in-javascript/47136113
 function setStyle(objId, propertyObject) {
   let element = document.getElementById(objId);
   for (var property in propertyObject) {
@@ -9,11 +9,21 @@ function setStyle(objId, propertyObject) {
 const grow = () => {
   let boxHeight = document.getElementById('box').style.height.slice(0, -2);
   let boxWidth = document.getElementById('box').style.width.slice(0, -2);
-
-  setStyle('box', { 'height':`${boxHeight * 1.1}px`, 'width':`${boxWidth * 1.1}px`, 'transition': 'height 1s ease-in-out, width 1s ease-in-out' });
+  setStyle('box', { 'height':`${boxHeight * 1.2}px`, 'width':`${boxWidth * 1.2}px`, 'transition': 'height .9s ease-in, width 1s ease-out' });
 }
 
-const changeColor = () => setStyle('box', {'background-color':'steelblue'});
+const getRandomRgbValues = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const changeColor = () => {
+  let r = getRandomRgbValues(0, 255);
+  let g = getRandomRgbValues(0, 255);
+  let b = getRandomRgbValues(0, 255);
+  setStyle('box', {'background-color':`rgb(${r}, ${g}, ${b})`, 'transition': 'background-color 0.75s'});
+}
 
 document.getElementById('box').style.opacity = 1;
 
@@ -22,16 +32,12 @@ const alanWalker = () => {
   if(boxOpacity <= 0) {
     boxOpacity = 1.5;
   }
-
-  setStyle('box', { 'opacity': boxOpacity - 0.5, 'transition': 'opacity 1s ease-in-out' });
+  setStyle('box', { 'opacity': boxOpacity - 0.5, 'transition': 'opacity .75s ease-in' });
 }
 
-const reset = () => {
-  setStyle('box', { 'height': '150px', 'width': '150px', 'opacity': 1, 'background-color': 'orange' })
-}
+const reset = () => setStyle('box', { 'height': '150px', 'width': '150px', 'opacity': 1, 'background-color': 'rgb(255, 165, 0)' });
 
 document.getElementById('button1').onclick = grow;
 document.getElementById('button2').onclick = changeColor;
 document.getElementById('button3').onclick = alanWalker;
 document.getElementById('button4').onclick = reset;
-
