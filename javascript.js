0ab25e7953e625b3cd5cc18ad5b714bd29222776
Robyn Navarro <rnavarro182@gmail.com>
2019-01-09 13:26:24 -0800

    color and fade button working. Added transition effects

--- javascript.js
+++ javascript.js
@@ -10,10 +10,23 @@ const grow = () => {
   let boxHeight = document.getElementById('box').style.height.slice(0, -2);
   let boxWidth = document.getElementById('box').style.width.slice(0, -2);
 
-  setStyle('box', {'height':`${boxHeight * 1.1}px`, 'width':`${boxWidth * 1.1}px`});
+  setStyle('box', { 'height':`${boxHeight * 1.1}px`, 'width':`${boxWidth * 1.1}px`, 'transition': 'height 1s ease-in-out, width 1s ease-in-out' });
 }
 
 const paintBlue = () => setStyle('box', {'background-color':'steelblue'});
 
+document.getElementById('box').style.opacity = 1;
+
+const alanWalker = () => {
+  let boxOpacity = document.getElementById('box').style.opacity;
+  if(boxOpacity <= 0) {
+    boxOpacity = 1.5;
+  }
+
+  setStyle('box', { 'opacity': boxOpacity - 0.5, 'transition': 'opacity 1s ease-in-out' });
+}
+
 document.getElementById('button1').onclick = grow;
 document.getElementById('button2').onclick = paintBlue;
+document.getElementById('button3').onclick = alanWalker;
+
